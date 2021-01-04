087575713c5588969660f286ba079f52cb85ba01
EndMad <70961155+EndMad@users.noreply.github.com>
2021-01-04 20:35:02 +0300

    Add files via upload

---
+++ Individual_12.py
@@ -0,0 +1,15 @@
+Monitor = float(input("Введите стоимость монитора"))
+S_blok = float(input("Введите стоимость системного блока"))
+Klava = float(input("Введите стоимость клавиатуры"))
+Mouse = float(input("Введите стоимость мышки"))
+
+Sum = Monitor + S_blok + Klava + Mouse
+
+Tri_comp = Sum * 3
+
+N = float(input("Введите кол-во компьютеров"))
+N_comp = Sum * N
+
+print("Cтоимость одного компьютера", Sum)
+print("Cтоимость трёх компьютеров", Tri_comp)
+print("Cтоимость N кол-ва компьютеров", N_comp)
