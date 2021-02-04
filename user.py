bf40d8d91f617cea413c7e78b97e1d9947dfc073
EndMad <70961155+EndMad@users.noreply.github.com>
2021-02-04 19:51:49 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,6 +1,12 @@
-User_name = input("What is your name? ")
-User_age = input("How old are you? ")
-User_liveplace = input("Where are you live? ")
-print("This is", User_name)
-print("It is", User_age)
-print("(S)he live in", User_liveplace)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+  User_name = input("What is your name? ")
+  User_age = input("How old are you? ")
+  User_liveplace = input("Where are you live? ")
+    print("This is", User_name)
+    print("It is", User_age)
+    print("(S)he live in", User_liveplace)
+
+    
